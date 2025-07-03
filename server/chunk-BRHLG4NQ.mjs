9ed3043226ed9c435aaadffc07497b9507f5bc7a
emag25 <emelyapraez25@gmail.com>
2025-07-02 22:10:17 -0500

    Auto-generated commit

---
+++ server/chunk-BRHLG4NQ.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{U as o,g as t}from"./chunk-RDXAN74U.mjs";var a=class e{loadingSubject=new t(!1);loading$=this.loadingSubject.asObservable();showLoading(){this.loadingSubject.next(!0)}hideLoading(){this.loadingSubject.next(!1)}static \u0275fac=function(i){return new(i||e)};static \u0275prov=o({token:e,factory:e.\u0275fac,providedIn:"root"})};export{a};
